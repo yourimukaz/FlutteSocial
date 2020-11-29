7cd944cacd83c86afedb68060bb2ed4d64d6d90f
yourimukaz <yourikatombmukaz@gmail.com>
2020-11-30 01:20:35 +0200

    Classe user

---
+++ lib/models/user.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttersocial/view/my_material.dart';
+
+class User {
+  String uid;
+  String name;
+  String surname;
+  String imageUrl;
+  List<dynamic> followers;
+  List<dynamic> following;
+  DocumentReference reference;
+  String documentId;
+
+  User(DocumentSnapshot snapshot) {
+    reference = snapshot.reference;
+    documentId = snapshot.id;
+    Map<String, dynamic> map = snapshot.data();
+    uid = map[keyUid];
+    name = map[keyName];
+    surname = map[keySurname];
+    followers = map[keyFollowers];
+    following = map[keyFollowing];
+    imageUrl = map[keyImageUrl];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      keyUid: uid,
+      keyName: name,
+      keySurname: surname,
+      keyFollowers: followers,
+      keyFollowing: following,
+      keyImageUrl: imageUrl
+    };
+  }
+}
